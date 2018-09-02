5aa250ca9749b4f221bb7e531f993b2744dd6b5b
Soranzo <40795992+Soranzo@users.noreply.github.com>
2018-09-02 12:00:53 +0200

    Update jupyter_notebook_config.py

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,1 +1,1 @@
-c.NotebookApp.tornado_settings = { 'headers': { 'Content-Security-Policy': "frame-ancestors 'self' https://blog.jupyter.org/" } }
+c.NotebookApp.tornado_settings = { 'headers': { 'Content-Security-Policy': "frame-ancestors 'self' http://mathalav.fr/" } }
